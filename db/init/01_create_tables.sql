c1d40df66b1eceeaaa5185a2fb46508fcd1a13a1
Hiro0803-HK <kd1339439@st.kobedenshi.ac.jp>
2023-11-29 11:59:45 +0900

    2023/11/29

---
+++ db/init/01_create_tables.sql
@@ -0,0 +1,6 @@
+create table books
+(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(100),
+    isnert_timestamp DATETIME DEFAULT NULL
+);
---
+++ db/init/02_insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO books VALUES(1, 'sample1', current_timestamp);
+INSERT INTO books VALUES(2, 'sample2', current_timestamp);
+INSERT INTO books VALUES(3, 'sample3', current_timestamp);
+INSERT INTO books VALUES(4, 'sample4', current_timestamp);
+INSERT INTO books VALUES(5, 'sample5', current_timestamp);
+
+
